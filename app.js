aec38a361b4fd928297b7c22acadbb1995f31380
SebastianSamayoa <samayoa_jhoan@hotmail.com>
2018-09-17 22:10:34 -0600

    agregando el CORS para las peticions del  Frontend

--- app.js
+++ app.js
@@ -9,6 +9,15 @@ var userRoutes = require('./routes/usuario');
 
 //Iniciando variables
 var app = express();
+
+//CORS
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE, OPTIONS");
+    next();
+});
+
 //Definindo puerto
 var puertonode = 3000;
 // parse application/x-www-form-urlencoded
--- routes/usuario.js
+++ routes/usuario.js
@@ -3,6 +3,9 @@ var Usuario = require('../models/usuario');
 
 var app = express();
 
+//================================================
+//Consulta de Usuarios
+//================================================
 app.get('/', (req, res) => {
     Usuario.find({}, (err, usuarios) => {
         if (err) {
@@ -18,6 +21,9 @@ app.get('/', (req, res) => {
     });
 });
 
+//================================================
+//Creacion de Usuario
+//================================================
 app.post('/', (req, res) => {
     var body = req.body;
     var usuario = new Usuario({
@@ -40,5 +46,49 @@ app.post('/', (req, res) => {
         });
     });
 });
+
+//================================================
+//Actualizacion de Usuario
+//================================================
+app.put('/:id', (req, res) => {
+    var body = req.body;
+    var id = req.params.id;
+    Usuario.findById(id, (err,usuario)=>{
+        if (err){
+            return res.status(500).json({
+                ok: false,
+                mensaje: 'Error al Buscar el Usuario',
+                errors: err
+            });
+        }
+
+        if ( !usuario ) {
+            return res.status(400).json({
+                ok: false,
+                mensaje: 'Error al Buscar el Usuario',
+                errors: { message: 'No existe el usuario buscado'}
+            });
+        }
+
+        usuario.nombre = body.nombre;
+        usuario.contrasena =  body.contrasena;
+
+        usuario.save((err, UserSave)=>{
+            if (err) {
+                return res.status(400).json({
+                    ok: false,
+                    mensaje: 'Error al Actualizar el Usuario',
+                    errors: err
+                });
+            }
+            return res.status(201).json({
+                ok: true,
+                mensaje: 'Usuario Actualizado con Exito',
+                usuarios: UserSave
+            });
+        });
+    });
+});
+
 
 module.exports = app;
